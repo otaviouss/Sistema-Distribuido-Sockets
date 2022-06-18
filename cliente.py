f50fb9dc0996f5172654ff761cc3961cf3450678
Otavio S. Gomes <otaviosantosg@gmail.com>
2022-06-18 11:56:17 -0300

    cliente criado & correções servidor

--- cliente.py
+++ cliente.py
@@ -1,19 +1,93 @@
 import socket
 
 class Cliente():
-    pass	
 
-# Create a socket object
-s = socket.socket()		
+    def __init__(self):
+        pass
 
-# Define the port on which you want to connect
-port = 8888			
+    def conectar(self):
+        s = socket.socket()
+        # Define the port on which you want to connect
+        port = 7777
+        # connect to the server on local computer
+        s.connect(('127.0.0.1', port))
+        return s
 
-# connect to the server on local computer
-s.connect(('127.0.0.1', port))
+    def solicitarCriarUsuario(self, email, nome, senha):
+        s = self.conectar()
 
-# receive data from the server and decoding to get the string.
-print (s.recv(1024).decode())
-# close the connection
-s.close()	
-	
+        s.sendall("CC:".encode())
+        s.sendall(email.encode())
+        s.sendall(":".encode())
+        s.sendall(nome.encode())
+        s.sendall(":".encode())
+        s.sendall(senha.encode())
+
+        s.close()
+
+    def cadastrarVoucher(self, titulo, descricao, gato, local, lanche, duracao, imagem, titular_id):
+        s = self.conectar()
+
+        s.sendall("CV:".encode())
+        s.sendall(titulo.encode())
+        s.sendall(":".encode())
+        s.sendall(descricao.encode())
+        s.sendall(":".encode())
+        s.sendall(gato.encode())
+        s.sendall(":".encode())
+        s.sendall(local.encode())
+        s.sendall(":".encode())
+        s.sendall(lanche.encode())
+        s.sendall(":".encode())
+        s.sendall(duracao.encode())
+        s.sendall(":".encode())
+        s.sendall(imagem.encode())
+        s.sendall(":".encode())
+        s.sendall(titular_id.encode())
+
+        s.close()
+
+    def proporTroca(self, id_voucher1, id_voucher2):
+        s = self.conectar()
+
+        s.sendall("PT:".encode())
+        s.sendall(id_voucher1.encode())
+        s.sendall(":".encode())
+        s.sendall(id_voucher2.encode())
+
+        s.close()
+
+    def realizarTroca(self, id_troca):
+        s = self.conectar()
+
+        s.sendall("RT:".encode())
+        s.sendall(id_troca.encode())
+
+        s.close()
+
+    def negarTroca(self, id_troca):
+        s = self.conectar()
+
+        s.sendall("NT:".encode())
+        s.sendall(id_troca.encode())
+
+        s.close()
+
+    def apresentarVouchers(self):
+        s = self.conectar()
+
+        s.sendall("AV".encode())
+
+        s.close()
+
+def main():
+    c = Cliente()
+    c.solicitarCriarUsuario(email="gm", nome="AAA", senha="123")	
+    c.cadastrarVoucher(titulo="TI", descricao="DS", gato="GT", local="LC", lanche="LA", duracao="DU", imagem="IM", titular_id="1")
+    c.proporTroca(id_voucher1="2", id_voucher2="4")
+    c.realizarTroca(id_troca="1")
+    c.negarTroca(id_troca="2")
+    c.apresentarVouchers()
+
+if __name__ == '__main__':
+    main()
--- servidor.py
+++ servidor.py
@@ -1,46 +1,84 @@
 import socket
+from bd import banco
+
+from _thread import *
+import asyncio
 
 class Servidor():
 
     def __init__():
         pass
 
+    async def cadastrarCliente(email, nome, senha):
+        await banco.inserir_usuario(email, nome, senha)
 
-# next create a socket object
-s = socket.socket()		
-print ("Socket successfully created")
+    async def cadastrarVoucher(titulo, descricao, gato, local, lanche, duracao, imagem, titular_id):
+        await banco.inserir_voucher(titulo, descricao, gato, local, lanche, duracao, imagem, int(titular_id))
 
-# reserve a port on your computer in our
-# case it is 12345 but it can be anything
-port = 8888			
+    async def proporTroca(id_voucher1, id_voucher2):
+        await banco.nova_Troca(int(id_voucher1), int(id_voucher2))
 
-# Next bind to the port
-# we have not typed any ip in the ip field
-# instead we have inputted an empty string
-# this makes the server listen to requests
-# coming from other computers on the network
-s.bind(('', port))		
-print ("socket binded to %s" %(port))
+    async def realizarTroca(id_troca):
+        await banco.alterar_Status_Troca_Aceito(int(id_troca))
 
-# put the socket into listening mode
-s.listen(5)	
-print ("socket is listening")		
+    async def negarTroca(id_troca):
+        banco.alterar_Status_Troca_Rejeitado(int(id_troca))
 
-# a forever loop until we interrupt it or
-# an error occurs
-while True:
-
-    # Establish connection with client.
-    c, addr = s.accept()	
-    print ('Got connection from', addr )
-
-    # send a thank you message to the client. encoding to send byte type.
-    c.send('Thank you for connecting'.encode())
-
-    # Close the connection with the client
-    c.close()
-
-    # Breaking once connection closed
-    # break
+    async def apresentarVouchers():
+        await banco.ver_vouchers()
 
 
+def main():
+    s = socket.socket()		
+    print ("Socket successfully created")
+
+    # reserve a port on your computer
+    port = 7777		
+
+    # Next bind to the port
+    # we have not typed any ip in the ip field
+    # instead we have inputted an empty string
+    # this makes the server listen to requests
+    # coming from other computers on the network
+    s.bind(('', port))		
+    print ("socket binded to %s" %(port))
+
+    # put the socket into listening mode
+    s.listen(5)	
+    print ("socket is listening")		
+
+    # a forever loop until we interrupt it or
+    # an error occurs
+    while True:
+
+        # Establish connection with client.
+        c, addr = s.accept()	
+        print ('Got connection from', addr )
+
+        op = c.recv(1024).decode().split(":")
+
+        if(op[0]=="CC"):
+            asyncio.run(Servidor.cadastrarCliente(op[1], op[2], op[3]))
+        elif(op[0]=="CV"):
+            asyncio.run(Servidor.cadastrarVoucher(op[1],op[2],op[3],op[4],op[5],op[6],op[7], op[8]))
+        elif(op[0]=="PT"):
+            asyncio.run(Servidor.proporTroca(op[1],op[2]))
+        elif(op[0]=="RT"):
+            asyncio.run(Servidor.realizarTroca(op[1],))
+        elif(op[0]=="NT"):
+            asyncio.run(Servidor.negarTroca(op[1],))
+        elif(op[0]=="AV"):
+            asyncio.run(Servidor.apresentarVouchers())
+
+        # send a thank you message to the client. encoding to send byte type.
+        c.send('Thank you for connecting'.encode())
+
+        # Close the connection with the client
+        c.close()
+
+        # Breaking once connection closed
+        # break
+
+
+if __name__ == '__main__':
+    main()
