7a812bcafe9a2201c8dc9e45f874149d41bdaa3a
Erian-A <erian.alves@ufv.br>
2022-06-25 16:54:48 -0300

    menu screen and voucher creation  screen

--- principal.py
+++ principal.py
@@ -5,6 +5,7 @@ kivy.require('2.1.0')
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.core.window import Window
 
 class Login(Screen):
     
@@ -21,22 +22,40 @@ class Login(Screen):
 
     def VerificarLogin(self, validado):
         if validado:
-            self.parent.current = 'telatrocas'
+            self.parent.current = 'telamenu'
         else:
             self.ids.Jovem.text = 'Não deu'
 
 
 
-class TelaTrocas(Screen):
+class TelaCadastro(Screen):
+
+    def CadastrarVoucher(self):
+        gato = self.ids.gato.text
+        titulo = self.ids.titulo.text
+        descricao = self.ids.descricao.text
+        local = self.ids.local.text
+        lanche = self.ids.lanche.text
+
+        if (gato != ''):
+            print(gato, titulo, descricao, local, lanche)
+            self.parent.current = 'telamenu'
+
+class TelaMenu(Screen):
+
+    def IrParaCadastro(self):
+        self.parent.current = 'telacadastro'
+
     pass
-
-
 
 class TestesInApp(App):
     def build(self):
+        Window.clearcolor = (1,1,1,1)
         manager = ScreenManager()
         manager.add_widget(Login(name='login'))
-        manager.add_widget(TelaTrocas(name='telatrocas'))
+        manager.add_widget(TelaMenu(name='telamenu'))
+        manager.add_widget(TelaCadastro(name='telacadastro'))
+
 
         return manager
 
